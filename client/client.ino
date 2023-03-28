5399eaab2e179b4056d7c9d241b7c399d42cac69
Sam Super <samsuper@Sams-Mac-mini.local>
2023-03-28 02:22:12 -0700

    Initial Commit

---
+++ client/client.ino
@@ -0,0 +1,186 @@
+// github.com/Xinyuan-LilyGO/T-Display-S3
+// In Arduino Preferences, on the Settings tab, enter the
+// https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+// 170 x 320
+
+#include "Arduino.h"
+#include "TFT_eSPI.h"
+#include "pin_config.h"
+TFT_eSPI tft = TFT_eSPI();
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+const char* ssid = "Hunter";
+const char* password = "saturday";
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://10.0.0.21/motion";
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5 * 1000;
+
+String sensorReadings;
+int BUTTON_1 = 0;
+int BUTTON_2 = 14;
+void IRAM_ATTR toggleButton1();
+void IRAM_ATTR toggleButton2();
+int POLL_INTERVAL = 100;
+
+void setup() {
+  pinMode(BUTTON_1, INPUT_PULLUP);
+  pinMode(BUTTON_2, INPUT_PULLUP);
+  // attachInterrupt( digitalPinToInterrupt( BUTTON_1 ), toggleButton1, FALLING);
+  // attachInterrupt( digitalPinToInterrupt( BUTTON_2 ), toggleButton2, FALLING);
+  // pinMode(PIN_POWER_ON, OUTPUT);
+  // digitalWrite(PIN_POWER_ON, HIGH);
+
+  Serial.begin(115200);
+  tft.begin();
+  tft.setRotation(3);
+  tft.setSwapBytes(true);
+
+  ledcSetup(0, 2000, 8);
+  ledcAttachPin(PIN_LCD_BL, 0);
+  ledcWrite(0, 255);
+
+  xTaskCreate(read_button_1, "Read Button 1", 2000, NULL, 1, NULL);
+  xTaskCreate(read_button_2, "Read Button 2", 2000, NULL, 1, NULL);
+}
+
+void checkConnections()
+{
+  if ( WiFi.status() != WL_CONNECTED )
+  {
+    setup_WiFi();
+  }
+}
+
+void setup_WiFi()
+{
+  tft.fillScreen( TFT_RED );
+  tft.setTextColor( TFT_BLACK, TFT_RED );
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println( ssid );
+  while ( WiFi.status() != WL_CONNECTED )
+  {
+    WiFi.begin( ssid, password);         //  07/08/2021  WiFi crashed, may have been stuck in a loop.
+    delay( 5000 );                      //  2000 is not enough but 3000 is.
+    Serial.println(".");
+  }
+  
+  if ( WiFi.status() == WL_CONNECTED )
+  {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    IPAddress broadCast = WiFi.localIP();
+    Serial.println( broadCast );
+    tft.setTextColor( TFT_WHITE, TFT_BLACK );
+    tft.fillScreen( TFT_GREEN );
+    tft.drawString("WiFi", 60, 0, 4);
+    tft.drawString(IpAddress2String(broadCast), 0, 50, 4);
+  }
+}
+
+String IpAddress2String(const IPAddress& ipAddress)
+{
+  return String(ipAddress[0]) + String(".") +\
+  String(ipAddress[1]) + String(".") +\
+  String(ipAddress[2]) + String(".") +\
+  String(ipAddress[3])  ; 
+}
+
+void read_button_1(void* parameter) {
+  for (;;) {
+    if (digitalRead(BUTTON_1) == 0) {
+      tft.fillScreen(TFT_BLUE);
+      tft.setTextColor(TFT_BLUE, TFT_GREEN);
+      tft.drawString("Waiting for Motion", 40, 20, 4);
+      Serial.println("Button 1 Pressed!");
+      delay(3000);
+    }
+    vTaskDelay(POLL_INTERVAL / portTICK_PERIOD_MS);
+  }
+}
+
+void read_button_2(void* parameter) {
+  for (;;) {
+    if (digitalRead(BUTTON_2) == 0) {
+      tft.fillScreen(TFT_BLUE);
+      tft.setTextColor(TFT_BLUE, TFT_GREEN);
+      tft.drawString("Waiting for Motion", 50, 20, 4);
+      Serial.println("Button 2 Pressed!");
+      delay(3000);
+    }
+    vTaskDelay(POLL_INTERVAL / portTICK_PERIOD_MS);
+  }
+}
+
+void loop() {
+  int lapse = millis() - lastTime;
+  Serial.print("lapse = ");
+  Serial.println(lapse);
+  checkConnections();
+
+  if (lapse > timerDelay)
+  {
+    lastTime = millis();
+    
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      sensorReadings = httpGETRequest(serverName);
+      Serial.println(sensorReadings);
+      JSONVar myObject = JSON.parse(sensorReadings);
+      Serial.print("json=");
+      Serial.println(myObject);
+
+      for (int i = 0; i < myObject.length(); i++)
+      {
+        int value_int = myObject[i]["value"];
+        String value_s = String(value_int);
+        Serial.print("value=");
+        Serial.println(value_s);
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextColor(TFT_RED, TFT_BLACK);
+        tft.drawString("MOTION  ALERT", 55, 30, 4);
+        tft.drawString(value_s, 100, 90, 8);
+        Serial.println(myObject[i]["time"]);
+        Serial.println(myObject[i]["type"]);
+        Serial.println(myObject[i]["value"]);
+        Serial.println(myObject[i]["unit"]);
+        delay(5000);
+      }
+      delay(2000);
+    } else {
+      Serial.println("WiFi Disconnected");
+    }
+  }
+  delay(1000);
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, serverName);
+  int httpResponseCode = http.GET();
+  Serial.print("httpResponseCode=");
+  Serial.println(httpResponseCode);
+  String payload = "{}";
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+  return payload;
+}
---
+++ server/server.ino
@@ -0,0 +1,418 @@
+// REST Motion
+// Features:
+// 1. T-Display
+//  github.com/Xinyuan-LilyGO/TTGO-T-Display
+// https://dl.espressif.com/dl/package_esp32_index.json
+//  esp32 -> ESP32 Dev Module
+
+//  04.01.2023
+//  Posts at regular intervals.
+//  Future Revisions:
+//    Decouple checking sensor & posting interval.
+//    Dual docs
+
+#include <TFT_eSPI.h>
+TFT_eSPI tft = TFT_eSPI();
+//https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
+//https://www.survivingwithandroid.com/esp32-rest-api-esp32-api-server/
+//https://www.mischianti.org/2020/05/24/rest-server-on-esp8266-and-esp32-get-and-json-formatter-part-2/
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
+// #include <Adafruit_Sensor.h>
+// #include <Adafruit_BME680.h>
+#include "time.h"
+const char* ntpServer = "pool.ntp.org";
+//const long  gmtOffset_sec = 3600 * 4;
+const long  gmtOffset_sec = 3600 * -8;
+const int   daylightOffset_sec = 3600;
+
+#define MAX_ELEMENTS 418 // 428 -> Crash
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define BUTTON1PIN 35
+#define BUTTON2PIN 0
+
+// Adafruit_BME680 bme; // I2C
+const char *SSID = "Hunter";
+const char *PWD = "saturday";
+const int red_pin = 5;
+const int green_pin = 18;
+const int blue_pin = 19;
+// Setting PWM frequency, channels and bit resolution
+const int frequency = 5000;
+const int redChannel = 0;
+const int greenChannel = 1;
+const int blueChannel = 2;
+const int resolution = 8;
+int SAMPLE_INTERVAL = 1 * 100;
+int POST_INTERVAL = 60 * 1000;
+
+WebServer server(80);
+StaticJsonDocument<10000> jsonDocument_1;
+StaticJsonDocument<10000> jsonDocument_2;
+
+char buffer[34000];
+float temperature;
+float humidity;
+float pressure;
+String printLocalTime();
+void setup_task();
+void getMenu();
+void getData();
+void resetData();
+void getTemperature();
+void getPressure();
+void getHumidity();
+void getSettings();
+void setup_routing();
+void read_sensor_data(void * parameter);
+void read_motion( char* device, int PIN );
+String get_time();
+
+
+void IRAM_ATTR toggleButton1()
+{
+  Serial.println("Button 1 Pressed!");
+  // tft.fillScreen(TFT_BLACK);
+  // tft.setCursor(0, 30);
+  // tft.drawLine(0, 35, 250, 35, TFT_BLUE);
+  // tft.setFreeFont(&Orbitron_Light_24);
+  // tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  // tft.print("Button 1");
+  // tft.setCursor(0, 60);
+  // tft.print("Pressed!");
+}
+
+void IRAM_ATTR toggleButton2()
+{
+  Serial.println("Button 2 Pressed!");
+  // tft.fillScreen(TFT_BLACK);
+  // tft.setCursor(0, 30);
+  // tft.drawLine(0, 35, 250, 35, TFT_BLUE);
+  // tft.setFreeFont(&Orbitron_Light_24);
+  // tft.setTextColor(TFT_GREEN, TFT_BLACK);
+  // tft.print("Button 2");
+  // tft.setCursor(0, 60);
+  // tft.print("Pressed!");
+}
+
+int ENFORCER = 27;   // PIN=27  Seco-Larm E-931-S35RRQ Enforcer Indoor/Outdoor Wall Mounted 
+// void IRAM_ATTR interrupt_motion_detected() {
+//   Serial.println("Interrupt - Motion Detected");
+//   // read_motion( "/enforcer", ENFORCER );
+//   read_motion( "/motion", ENFORCER );
+// }
+
+
+unsigned long start_time = 0;
+unsigned long last_time = 0;
+bool motion;
+int motion_count = 0;
+int motion_count_post = 0;
+int motion_threshold = 1;
+unsigned long interval = 5000;      //  Allowed time to pass with no motion.
+int BUTTON_1 = 35;
+int BUTTON_2 = 0;
+
+void setup()
+{
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLUE);
+  pinMode(BUTTON1PIN, INPUT);
+  pinMode(BUTTON2PIN, INPUT);
+  attachInterrupt(BUTTON1PIN, toggleButton1, FALLING);
+  attachInterrupt(BUTTON2PIN, toggleButton2, FALLING);
+  if (ENFORCER)
+  {
+        pinMode(ENFORCER, INPUT_PULLUP);       // Enforcer works beautifully with a INPUT_PULLUP
+        // attachInterrupt(ENFORCER, interrupt_motion_detected, FALLING );
+  }
+
+  Serial.begin(115200);
+  // while (!Serial);
+  Serial.print("Connecting to Wi-Fi");
+  WiFi.begin(SSID, PWD);
+  // int t = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    // Serial.print(t);
+    delay(500);
+    // t += 1;
+    // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+  }
+
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+  Serial.print("Connected! IP Address: ");
+  Serial.println(WiFi.localIP());
+  xTaskCreate(read_sensor_data, "Read sensor data", 2000, NULL, 2, NULL);
+  xTaskCreate(post_sensor_data, "Post sensor data", 2000, NULL, 1, NULL);
+  setup_routing();
+}
+
+void setup_routing()
+{
+  server.on("/", getMenu);
+  server.on("/poll", get_poll);
+  server.on("/motion", get_motion);
+  server.on("/reset", resetData);
+  server.on(F("/set"), HTTP_GET, getSettings);
+  server.begin();
+}
+
+void read_sensors()
+{ 
+  read_motion( "/motion", ENFORCER );
+}
+
+//  Read Boolean Motion
+void read_motion( char* device, int PIN )
+{
+  // Serial.print("motion_count = ");
+  if ( motion_count > 0 )
+  {
+    int lapse = millis() - last_time;
+    if ( lapse > interval )
+    {
+            add_json_object_2("motion", motion_count, "i");
+
+      motion_count = 0;
+      // t.fill_screen( TFT_BLACK );
+      Serial.println("read_motion: motion_count = 0");
+    }
+  }
+
+  motion = digitalRead( PIN );
+  if ( motion )
+  {
+    if ( motion_count == 0 )
+    {
+        start_time = millis();
+        // t.fill_screen( TFT_BLACK );
+    }
+    last_time = millis();
+    //    t.lcd_display( 1, 0, t.fontsize, 100, String( last_time ) );
+    motion_count++;
+    motion_count_post++;
+    String mt = "Motion=" + String( motion_count );
+    // t.lcd_display( 1, 30, t.fontsize, 0, mt );
+    Serial.println(mt);
+    // if ( motion_count >= motion_threshold )
+    // {
+    //     // publish_message( device, String( motion_count ) );
+    //     Serial.println("publish_message");
+    // }
+  }
+}
+
+
+void getMenu()
+{
+  Serial.println("get Menu");
+  String html = "<html><body>";
+  html += "<h1>Motion Sensor - Enforcer</h1><br>";
+  html += "Interval = ";
+  html += SAMPLE_INTERVAL;
+  html += "<br>";
+  html += "<a href='http://10.0.0.21/set?interval=10000'>http://10.0.0.21/set?interval=10000</a><br>";
+  html += "<a href='http://10.0.0.21/motion'>Motion Data</a><br>";
+  html += "<a href='http://10.0.0.21/poll'>Polled Data</a><br>";  
+  html += "<a href='http://10.0.0.21/reset'>Reset</a><br>";
+  html += "</body></html>";
+  server.send( 200, "text/html", html );
+}
+
+
+void getSettings()
+{
+    Serial.println("getSettings()");
+    // Serial.println( server.args() );
+    String message = "";
+    for ( uint8_t i = 0; i < server.args(); i++ )
+    {
+      message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
+      String name1 = server.argName ( i );
+      //      String value1 = server.arg ( i );
+      if ( name1.equals("interval") )
+      {
+        SAMPLE_INTERVAL = server.arg ( i ).toInt();
+        Serial.println( SAMPLE_INTERVAL );
+      }
+    }
+    Serial.println( message );
+    //    if (server.arg("interval")== "true")
+    //    {
+    //        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
+    //    }
+    DynamicJsonDocument doc1(2048);
+    doc1["set"] = "Great Success !";
+    // Serialize JSON document
+    String json;
+    serializeJson(doc1, json);
+    server.send(200, "application/json", json);
+}
+
+
+
+void add_json_object(char *tag, float value, char *unit)
+{
+  int dic_size = jsonDocument_1.size();
+  if (dic_size >= MAX_ELEMENTS)
+  {
+      Serial.println("Remove 1");
+      jsonDocument_1.remove(0);
+  }
+  JsonObject objt = jsonDocument_1.createNestedObject();
+  objt["time"] = printLocalTime();
+  objt["type"] = tag;
+  objt["value"] = value;
+  objt["unit"] = unit;
+}
+
+void add_json_object_2(char *tag, float value, char *unit)
+{
+  int dic_size = jsonDocument_2.size();
+  if (dic_size >= MAX_ELEMENTS)
+  {
+      Serial.println("Remove 1");
+      jsonDocument_2.remove(0);
+  }
+  JsonObject objt = jsonDocument_2.createNestedObject();
+  objt["time"] = printLocalTime();
+  objt["type"] = tag;
+  objt["value"] = value;
+  objt["unit"] = unit;
+}
+
+void show_time()
+{
+  String t = get_time();
+      tft.fillScreen(TFT_BLACK);
+      tft.setTextColor(TFT_BLUE);
+      tft.drawString(t, 10, 10, 6);
+      Serial.print("time=");
+  Serial.println(t);
+}
+
+void read_sensor_data(void * parameter)
+{
+   for (;;)
+   {
+      String theString = get_time();
+      // Serial.print("theString=");
+      // Serial.println(theString);
+      int theInt = theString.substring(theString.length() - 2, theString.length()).toInt();
+      // Serial.print("theInt=");
+      // Serial.println(theInt);
+        read_sensors();
+        vTaskDelay(SAMPLE_INTERVAL / portTICK_PERIOD_MS);
+   }
+}
+
+void post_sensor_data(void * parameter)
+{
+   for (;;)
+   {
+      add_json_object("post", motion_count_post, "i");
+      motion_count_post = 0;
+      vTaskDelay(POST_INTERVAL);
+   }
+}
+
+void resetData()
+{
+    jsonDocument_1.clear();
+    jsonDocument_2.clear();
+    Serial.println("Data reset");
+    DynamicJsonDocument doc1(2048);
+    doc1["reset"] = "Great Success !";
+    String json;
+    serializeJson(doc1, json);
+    server.send(200, "application/json", json);
+}
+
+void get_poll()
+{
+  Serial.println("Get Polled Data");
+  serializeJson(jsonDocument_1, buffer);
+  Serial.println( buffer );
+  // int dic_size_motion = jsonDocument_1.size();
+  // int dic_size = doc.size();
+  // Serial.println(dic_size_motion);
+  // Serial.println(dic_size);
+
+  // for (int i=0; i < dic_size; i++)
+  // {
+  //   const char* sensor = doc[i]["type"];
+  //   Serial.println( sensor );
+  // }
+  // Serial.println( buffer );
+  server.send(200, "application/json", buffer);
+}
+
+
+void get_motion()
+{
+  Serial.println("Get Motion Times");
+  StaticJsonDocument<64> filter;
+  filter["type"] = "motion";
+  serializeJson(jsonDocument_2, buffer);
+  server.send(200, "application/json", buffer);
+  jsonDocument_2.clear();     //  Reset data for iot client device
+}
+
+
+void loop()
+{
+  server.handleClient();
+}
+
+
+String printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return "Error";
+  }
+  //  Serial.println(&timeinfo, "print - %A, %B %d %Y %H:%M:%S");
+  //https://arduino.stackexchange.com/questions/52676/how-do-you-convert-a-formatted-print-statement-into-a-string-variable
+  char timeStringBuff[50]; //50 chars should be enough
+  strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
+  //print like "const char*"
+  //  Serial.println(timeStringBuff);
+  //Optional: Construct String object
+  String asString(timeStringBuff);
+  return asString;
+}
+
+
+String get_time()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return "Error";
+  }
+  //  Serial.println(&timeinfo, "print - %A, %B %d %Y %H:%M:%S");
+
+  //https://arduino.stackexchange.com/questions/52676/how-do-you-convert-a-formatted-print-statement-into-a-string-variable
+  char timeStringBuff[50]; //50 chars should be enough
+  strftime(timeStringBuff, sizeof(timeStringBuff), "%H:%M:%S", &timeinfo);
+  //print like "const char*"
+  //  Serial.println(timeStringBuff);
+  //Optional: Construct String object
+  String asString(timeStringBuff);
+  return asString;
+}
