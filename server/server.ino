e2cca5fa6306a1b0f69a2ffca900fa2e74d10430
2021sam <7050592@gmail.com>
2023-03-30 11:39:54 -0700

    Enable return {}

--- server/server.ino
+++ server/server.ino
@@ -24,12 +24,12 @@ TFT_eSPI tft = TFT_eSPI();
 // #include <Adafruit_Sensor.h>
 // #include <Adafruit_BME680.h>
 #include "time.h"
-const char* ntpServer = "pool.ntp.org";
+const char *ntpServer = "pool.ntp.org";
 //const long  gmtOffset_sec = 3600 * 4;
-const long  gmtOffset_sec = 3600 * -8;
-const int   daylightOffset_sec = 3600;
+const long gmtOffset_sec = 3600 * -8;
+const int daylightOffset_sec = 3600;
 
-#define MAX_ELEMENTS 418 // 428 -> Crash
+#define MAX_ELEMENTS 418  // 428 -> Crash
 #define BME_SCK 13
 #define BME_MISO 12
 #define BME_MOSI 11
@@ -71,13 +71,12 @@ void getPressure();
 void getHumidity();
 void getSettings();
 void setup_routing();
-void read_sensor_data(void * parameter);
-void read_motion( char* device, int PIN );
+void read_sensor_data(void *parameter);
+void read_motion(char *device, int PIN);
 String get_time();
 
 
-void IRAM_ATTR toggleButton1()
-{
+void IRAM_ATTR toggleButton1() {
   Serial.println("Button 1 Pressed!");
   // tft.fillScreen(TFT_BLACK);
   // tft.setCursor(0, 30);
@@ -89,8 +88,7 @@ void IRAM_ATTR toggleButton1()
   // tft.print("Pressed!");
 }
 
-void IRAM_ATTR toggleButton2()
-{
+void IRAM_ATTR toggleButton2() {
   Serial.println("Button 2 Pressed!");
   // tft.fillScreen(TFT_BLACK);
   // tft.setCursor(0, 30);
@@ -102,7 +100,7 @@ void IRAM_ATTR toggleButton2()
   // tft.print("Pressed!");
 }
 
-int ENFORCER = 27;   // PIN=27  Seco-Larm E-931-S35RRQ Enforcer Indoor/Outdoor Wall Mounted 
+int ENFORCER = 27;  // PIN=27  Seco-Larm E-931-S35RRQ Enforcer Indoor/Outdoor Wall Mounted
 // void IRAM_ATTR interrupt_motion_detected() {
 //   Serial.println("Interrupt - Motion Detected");
 //   // read_motion( "/enforcer", ENFORCER );
@@ -116,12 +114,11 @@ bool motion;
 int motion_count = 0;
 int motion_count_post = 0;
 int motion_threshold = 1;
-unsigned long interval = 5000;      //  Allowed time to pass with no motion.
+unsigned long interval = 5000;  //  Allowed time to pass with no motion.
 int BUTTON_1 = 35;
 int BUTTON_2 = 0;
 
-void setup()
-{
+void setup() {
   tft.init();
   tft.setRotation(1);
   tft.fillScreen(TFT_BLUE);
@@ -129,10 +126,9 @@ void setup()
   pinMode(BUTTON2PIN, INPUT);
   attachInterrupt(BUTTON1PIN, toggleButton1, FALLING);
   attachInterrupt(BUTTON2PIN, toggleButton2, FALLING);
-  if (ENFORCER)
-  {
-        pinMode(ENFORCER, INPUT_PULLUP);       // Enforcer works beautifully with a INPUT_PULLUP
-        // attachInterrupt(ENFORCER, interrupt_motion_detected, FALLING );
+  if (ENFORCER) {
+    pinMode(ENFORCER, INPUT_PULLUP);  // Enforcer works beautifully with a INPUT_PULLUP
+                                      // attachInterrupt(ENFORCER, interrupt_motion_detected, FALLING );
   }
 
   Serial.begin(115200);
@@ -140,14 +136,13 @@ void setup()
   Serial.print("Connecting to Wi-Fi");
   WiFi.begin(SSID, PWD);
   // int t = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     // Serial.print(t);
-    delay(500);
+    delay(5000);
     // t += 1;
     // Init and get the time
-  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-  printLocalTime();
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    printLocalTime();
   }
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
@@ -159,8 +154,7 @@ void setup()
   setup_routing();
 }
 
-void setup_routing()
-{
+void setup_routing() {
   server.on("/", getMenu);
   server.on("/poll", get_poll);
   server.on("/motion", get_motion);
@@ -169,21 +163,17 @@ void setup_routing()
   server.begin();
 }
 
-void read_sensors()
-{ 
-  read_motion( "/motion", ENFORCER );
+void read_sensors() {
+  read_motion("/motion", ENFORCER);
 }
 
 //  Read Boolean Motion
-void read_motion( char* device, int PIN )
-{
+void read_motion(char *device, int PIN) {
   // Serial.print("motion_count = ");
-  if ( motion_count > 0 )
-  {
+  if (motion_count > 0) {
     int lapse = millis() - last_time;
-    if ( lapse > interval )
-    {
-            add_json_object_2("motion", motion_count, "i");
+    if (lapse > interval) {
+      add_json_object_2("motion", motion_count, "i");
 
       motion_count = 0;
       // t.fill_screen( TFT_BLACK );
@@ -191,19 +181,17 @@ void read_motion( char* device, int PIN )
     }
   }
 
-  motion = digitalRead( PIN );
-  if ( motion )
-  {
-    if ( motion_count == 0 )
-    {
-        start_time = millis();
-        // t.fill_screen( TFT_BLACK );
+  motion = digitalRead(PIN);
+  if (motion) {
+    if (motion_count == 0) {
+      start_time = millis();
+      // t.fill_screen( TFT_BLACK );
     }
     last_time = millis();
     //    t.lcd_display( 1, 0, t.fontsize, 100, String( last_time ) );
     motion_count++;
     motion_count_post++;
-    String mt = "Motion=" + String( motion_count );
+    String mt = "Motion=" + String(motion_count);
     // t.lcd_display( 1, 30, t.fontsize, 0, mt );
     Serial.println(mt);
     // if ( motion_count >= motion_threshold )
@@ -215,61 +203,55 @@ void read_motion( char* device, int PIN )
 }
 
 
-void getMenu()
-{
+void getMenu() {
   Serial.println("get Menu");
   String html = "<html><body>";
-  html += "<h1>Motion Sensor - Enforcer</h1><br>";
+  html += "<h1>Motion Sensor - Enforcer 1</h1><br>";
   html += "Interval = ";
   html += SAMPLE_INTERVAL;
   html += "<br>";
   html += "<a href='http://10.0.0.21/set?interval=10000'>http://10.0.0.21/set?interval=10000</a><br>";
   html += "<a href='http://10.0.0.21/motion'>Motion Data</a><br>";
-  html += "<a href='http://10.0.0.21/poll'>Polled Data</a><br>";  
+  html += "<a href='http://10.0.0.21/poll'>Polled Data</a><br>";
   html += "<a href='http://10.0.0.21/reset'>Reset</a><br>";
   html += "</body></html>";
-  server.send( 200, "text/html", html );
+  server.send(200, "text/html", html);
 }
 
 
-void getSettings()
-{
-    Serial.println("getSettings()");
-    // Serial.println( server.args() );
-    String message = "";
-    for ( uint8_t i = 0; i < server.args(); i++ )
-    {
-      message += " " + server.argName ( i ) + ": " + server.arg ( i ) + "\n";
-      String name1 = server.argName ( i );
-      //      String value1 = server.arg ( i );
-      if ( name1.equals("interval") )
-      {
-        SAMPLE_INTERVAL = server.arg ( i ).toInt();
-        Serial.println( SAMPLE_INTERVAL );
-      }
+void getSettings() {
+  Serial.println("getSettings()");
+  // Serial.println( server.args() );
+  String message = "";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    String name1 = server.argName(i);
+    //      String value1 = server.arg ( i );
+    if (name1.equals("interval")) {
+      SAMPLE_INTERVAL = server.arg(i).toInt();
+      Serial.println(SAMPLE_INTERVAL);
     }
-    Serial.println( message );
-    //    if (server.arg("interval")== "true")
-    //    {
-    //        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
-    //    }
-    DynamicJsonDocument doc1(2048);
-    doc1["set"] = "Great Success !";
-    // Serialize JSON document
-    String json;
-    serializeJson(doc1, json);
-    server.send(200, "application/json", json);
+  }
+  Serial.println(message);
+  //    if (server.arg("interval")== "true")
+  //    {
+  //        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
+  //    }
+  DynamicJsonDocument doc1(2048);
+  doc1["set"] = "Great Success !";
+  // Serialize JSON document
+  String json;
+  serializeJson(doc1, json);
+  server.send(200, "application/json", json);
 }
 
 
 
-void add_json_object(char *tag, float value, char *unit)
-{
+void add_json_object(char *tag, float value, char *unit) {
   int dic_size = jsonDocument_1.size();
-  if (dic_size >= MAX_ELEMENTS)
-  {
-      Serial.println("Remove 1");
-      jsonDocument_1.remove(0);
+  if (dic_size >= MAX_ELEMENTS) {
+    Serial.println("Remove 1");
+    jsonDocument_1.remove(0);
   }
   JsonObject objt = jsonDocument_1.createNestedObject();
   objt["time"] = printLocalTime();
@@ -278,13 +260,11 @@ void add_json_object(char *tag, float value, char *unit)
   objt["unit"] = unit;
 }
 
-void add_json_object_2(char *tag, float value, char *unit)
-{
+void add_json_object_2(char *tag, float value, char *unit) {
   int dic_size = jsonDocument_2.size();
-  if (dic_size >= MAX_ELEMENTS)
-  {
-      Serial.println("Remove 1");
-      jsonDocument_2.remove(0);
+  if (dic_size >= MAX_ELEMENTS) {
+    Serial.println("Remove 1");
+    jsonDocument_2.remove(0);
   }
   JsonObject objt = jsonDocument_2.createNestedObject();
   objt["time"] = printLocalTime();
@@ -293,58 +273,51 @@ void add_json_object_2(char *tag, float value, char *unit)
   objt["unit"] = unit;
 }
 
-void show_time()
-{
+void show_time() {
   String t = get_time();
-      tft.fillScreen(TFT_BLACK);
-      tft.setTextColor(TFT_BLUE);
-      tft.drawString(t, 10, 10, 6);
-      Serial.print("time=");
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString(t, 10, 10, 6);
+  Serial.print("time=");
   Serial.println(t);
 }
 
-void read_sensor_data(void * parameter)
-{
-   for (;;)
-   {
-      String theString = get_time();
-      // Serial.print("theString=");
-      // Serial.println(theString);
-      int theInt = theString.substring(theString.length() - 2, theString.length()).toInt();
-      // Serial.print("theInt=");
-      // Serial.println(theInt);
-        read_sensors();
-        vTaskDelay(SAMPLE_INTERVAL / portTICK_PERIOD_MS);
-   }
+void read_sensor_data(void *parameter) {
+  for (;;) {
+    String theString = get_time();
+    // Serial.print("theString=");
+    // Serial.println(theString);
+    int theInt = theString.substring(theString.length() - 2, theString.length()).toInt();
+    // Serial.print("theInt=");
+    // Serial.println(theInt);
+    read_sensors();
+    vTaskDelay(SAMPLE_INTERVAL / portTICK_PERIOD_MS);
+  }
 }
 
-void post_sensor_data(void * parameter)
-{
-   for (;;)
-   {
-      add_json_object("post", motion_count_post, "i");
-      motion_count_post = 0;
-      vTaskDelay(POST_INTERVAL);
-   }
+void post_sensor_data(void *parameter) {
+  for (;;) {
+    add_json_object("post", motion_count_post, "i");
+    motion_count_post = 0;
+    vTaskDelay(POST_INTERVAL);
+  }
 }
 
-void resetData()
-{
-    jsonDocument_1.clear();
-    jsonDocument_2.clear();
-    Serial.println("Data reset");
-    DynamicJsonDocument doc1(2048);
-    doc1["reset"] = "Great Success !";
-    String json;
-    serializeJson(doc1, json);
-    server.send(200, "application/json", json);
+void resetData() {
+  jsonDocument_1.clear();
+  jsonDocument_2.clear();
+  Serial.println("Data reset");
+  DynamicJsonDocument doc1(2048);
+  doc1["reset"] = "Great Success !";
+  String json;
+  serializeJson(doc1, json);
+  server.send(200, "application/json", json);
 }
 
-void get_poll()
-{
+void get_poll() {
   Serial.println("Get Polled Data");
   serializeJson(jsonDocument_1, buffer);
-  Serial.println( buffer );
+  Serial.println(buffer);
   // int dic_size_motion = jsonDocument_1.size();
   // int dic_size = doc.size();
   // Serial.println(dic_size_motion);
@@ -360,34 +333,42 @@ void get_poll()
 }
 
 
-void get_motion()
-{
+void get_motion() {
   Serial.println("Get Motion Times");
   StaticJsonDocument<64> filter;
   filter["type"] = "motion";
-  serializeJson(jsonDocument_2, buffer);
-  server.send(200, "application/json", buffer);
-  jsonDocument_2.clear();     //  Reset data for iot client device
+
+  int size = jsonDocument_2.size();
+  Serial.println(size);
+  if (!size) {
+    Serial.println("NULL");
+    server.send(200, "application/json", "{}");
+  }
+
+
+  if (size) {
+    Serial.println("Not NULL");
+    serializeJson(jsonDocument_2, buffer);
+    server.send(200, "application/json", buffer);
+    jsonDocument_2.clear();  //  Reset data for iot client device
+  }
 }
 
 
-void loop()
-{
+void loop() {
   server.handleClient();
 }
 
 
-String printLocalTime()
-{
+String printLocalTime() {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo))
-  {
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return "Error";
   }
   //  Serial.println(&timeinfo, "print - %A, %B %d %Y %H:%M:%S");
   //https://arduino.stackexchange.com/questions/52676/how-do-you-convert-a-formatted-print-statement-into-a-string-variable
-  char timeStringBuff[50]; //50 chars should be enough
+  char timeStringBuff[50];  //50 chars should be enough
   strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
   //print like "const char*"
   //  Serial.println(timeStringBuff);
@@ -397,18 +378,16 @@ String printLocalTime()
 }
 
 
-String get_time()
-{
+String get_time() {
   struct tm timeinfo;
-  if(!getLocalTime(&timeinfo))
-  {
+  if (!getLocalTime(&timeinfo)) {
     Serial.println("Failed to obtain time");
     return "Error";
   }
   //  Serial.println(&timeinfo, "print - %A, %B %d %Y %H:%M:%S");
 
   //https://arduino.stackexchange.com/questions/52676/how-do-you-convert-a-formatted-print-statement-into-a-string-variable
-  char timeStringBuff[50]; //50 chars should be enough
+  char timeStringBuff[50];  //50 chars should be enough
   strftime(timeStringBuff, sizeof(timeStringBuff), "%H:%M:%S", &timeinfo);
   //print like "const char*"
   //  Serial.println(timeStringBuff);
