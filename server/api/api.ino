25a7ab45e72cdb1aece683ae861d3826bd3e3047
2021sam <7050592@gmail.com>
2024-06-20 16:30:29 -0700

    See current temperature data

--- server/api/api.ino
+++ server/api/api.ino
@@ -1,44 +1,21 @@
-// REST Motion
-// Features:
-// 1. T-Display
-//  github.com/Xinyuan-LilyGO/TTGO-T-Display
-// https://dl.espressif.com/dl/package_esp32_index.json
-//  esp32 -> ESP32 Dev Module
-
-//  04.01.2023
-//  Posts at regular intervals.
-//  Future Revisions:
-//    Decouple checking sensor & posting interval.
-//    Dual docs
-
-
 #include <WiFi.h>
 #include <FS.h>
 #include <HTTPClient.h>
 #include "time.h"
-
-
 #include <TFT_eSPI.h>
-TFT_eSPI tft = TFT_eSPI();
 #include <SPI.h>
-//https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
-//https://www.survivingwithandroid.com/esp32-rest-api-esp32-api-server/
-//https://www.mischianti.org/2020/05/24/rest-server-on-esp8266-and-esp32-get-and-json-formatter-part-2/
 #include <Arduino.h>
-// #include <WiFi.h>
-// #include <FS.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
 #include <Wire.h>
-// #include <Adafruit_Sensor.h>
-// #include <Adafruit_BME680.h>
-// #include "time.h"
+#include "Adafruit_BME680.h"
+
+TFT_eSPI tft = TFT_eSPI();
 const char *ntpServer = "pool.ntp.org";
-//const long  gmtOffset_sec = 3600 * 4;
 const long gmtOffset_sec = 3600 * -8;
 const int daylightOffset_sec = 3600;
 
-#define MAX_ELEMENTS 418  // 428 -> Crash
+#define MAX_ELEMENTS 418
 #define BME_SCK 13
 #define BME_MISO 12
 #define BME_MOSI 11
@@ -47,13 +24,11 @@ const int daylightOffset_sec = 3600;
 #define BUTTON1PIN 35
 #define BUTTON2PIN 0
 
-// Adafruit_BME680 bme; // I2C
 const char *SSID = "IOT";
 const char *PWD = "saturday";
 const int red_pin = 5;
 const int green_pin = 18;
 const int blue_pin = 19;
-// Setting PWM frequency, channels and bit resolution
 const int frequency = 5000;
 const int redChannel = 0;
 const int greenChannel = 1;
@@ -65,18 +40,21 @@ int POST_INTERVAL = 60 * 1000;
 WebServer server(80);
 StaticJsonDocument<10000> jsonDocument_1;
 StaticJsonDocument<10000> jsonDocument_2;
-
 char buffer[34000];
-float temperature;
-float humidity;
-float pressure;
+bool motion;
+int motion_count = 0;
+int motion_count_post = 0;
+int motion_threshold = 1;
+unsigned long start_time = 0;
+unsigned long last_time = 0;
+unsigned long interval = 5000;
+
+Adafruit_BME680 bme;
+
 void setup_task();
 void getMenu();
 void getData();
 void resetData();
-void getTemperature();
-void getPressure();
-void getHumidity();
 void getSettings();
 void setup_routing();
 void read_sensor_data(void *parameter);
@@ -84,55 +62,14 @@ void read_motion(char *device, int PIN);
 void setup_bme();
 void loop_bme();
 String get_time();
-
-
-void IRAM_ATTR toggleButton1() {
-  Serial.println("Button 1 Pressed!");
-  // tft.fillScreen(TFT_BLACK);
-  // tft.setCursor(0, 30);
-  // tft.drawLine(0, 35, 250, 35, TFT_BLUE);
-  // tft.setFreeFont(&Orbitron_Light_24);
-  // tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  // tft.print("Button 1");
-  // tft.setCursor(0, 60);
-  // tft.print("Pressed!");
-}
-
-void IRAM_ATTR toggleButton2() {
-  Serial.println("Button 2 Pressed!");
-  // tft.fillScreen(TFT_BLACK);
-  // tft.setCursor(0, 30);
-  // tft.drawLine(0, 35, 250, 35, TFT_BLUE);
-  // tft.setFreeFont(&Orbitron_Light_24);
-  // tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  // tft.print("Button 2");
-  // tft.setCursor(0, 60);
-  // tft.print("Pressed!");
-}
-
-int ENFORCER = 27;  // PIN=27  Seco-Larm E-931-S35RRQ Enforcer Indoor/Outdoor Wall Mounted
-// void IRAM_ATTR interrupt_motion_detected() {
-//   Serial.println("Interrupt - Motion Detected");
-//   // read_motion( "/enforcer", ENFORCER );
-//   read_motion( "/motion", ENFORCER );
-// }
-
-
-unsigned long start_time = 0;
-unsigned long last_time = 0;
-bool motion;
-int motion_count = 0;
-int motion_count_post = 0;
-int motion_threshold = 1;
-
-int temperature = 0;
-int humidity = 0;
-int gas = 0;
-
-
-unsigned long interval = 5000;  //  Allowed time to pass with no motion.
-int BUTTON_1 = 35;
-int BUTTON_2 = 0;
+void IRAM_ATTR toggleButton1();
+void IRAM_ATTR toggleButton2();
+void add_json_object(char *tag, float value, char *unit);
+void add_json_object_2(char *tag, float value, char *unit);
+void read_sensors();
+void get_poll();
+void get_motion();
+void get_sensor_values();
 
 void setup() {
   tft.init();
@@ -142,25 +79,17 @@ void setup() {
   pinMode(BUTTON2PIN, INPUT);
   attachInterrupt(BUTTON1PIN, toggleButton1, FALLING);
   attachInterrupt(BUTTON2PIN, toggleButton2, FALLING);
-  // if (ENFORCER) {
-  //   pinMode(ENFORCER, INPUT_PULLUP);  // Enforcer works beautifully with a INPUT_PULLUP
-  //                                     // attachInterrupt(ENFORCER, interrupt_motion_detected, FALLING );
-  // }
 
   Serial.begin(115200);
-  // while (!Serial);
+
   Serial.print("Connecting to Wi-Fi...");
   WiFi.begin(SSID, PWD);
-  // while (WiFi.status() != WL_CONNECTED) {
-  //   delay(5000);
-  //   }
 
   unsigned long startAttemptTime = millis();
   while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 30000) {
       Serial.print(".");
       delay(500);
-    }
-
+  }
 
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("Failed to connect to Wi-Fi");
@@ -169,25 +98,14 @@ void setup() {
   Serial.print("Connected! IP Address: ");
   Serial.println(WiFi.localIP());
 
-
-  // Check internet access
-  if (checkInternetAccess()) {
-    Serial.println("Internet access verified");
-  } else {
-    Serial.println("No internet access");
-    while (true); // Stop here if no internet access
-  }
-
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  // Wait for time to be set
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo, 10000)) {  // Increase timeout to 10 seconds
     Serial.println("Failed to obtain time");
     return;
   }
   Serial.println(&timeinfo, "Time set: %A, %B %d %Y %H:%M:%S");
-
 
   xTaskCreate(read_sensor_data, "Read sensor data", 2000, NULL, 2, NULL);
   xTaskCreate(post_sensor_data, "Post sensor data", 2000, NULL, 1, NULL);
@@ -199,154 +117,106 @@ void setup_routing() {
   server.on("/", getMenu);
   server.on("/poll", get_poll);
   server.on("/motion", get_motion);
-  server.on("/t", get_temperature);
+  server.on("/temp", get_sensor_values);
   server.on("/reset", resetData);
   server.on(F("/set"), HTTP_GET, getSettings);
   server.begin();
 }
 
-
-bool checkInternetAccess() {
-  HTTPClient http;
-  http.begin("http://google.com");  // URL for checking internet access
-  int httpCode = http.GET();
-  http.end();
-  return httpCode > 0;
-}
-
-
-
-void read_sensors() {
-  read_motion("/motion", ENFORCER);
-}
-
-//  Read Boolean Motion
-void read_motion(char *device, int PIN) {
-  // Serial.print("motion_count = ");
-  if (motion_count > 0) {
-    int lapse = millis() - last_time;
-    if (lapse > interval) {
-      add_json_object_2("motion", motion_count, "i");
-
-      motion_count = 0;
-      // t.fill_screen( TFT_BLACK );
-      Serial.println("read_motion: motion_count = 0");
-    }
+void get_sensor_values() {
+  unsigned long endTime = bme.beginReading();
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading BME680 :("));
+    server.send(500, "application/json", "{\"error\": \"Failed to start sensor reading\"}");
+    return;
+  }
+  delay(50);
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading BME680 :("));
+    server.send(500, "application/json", "{\"error\": \"Failed to complete sensor reading\"}");
+    return;
   }
 
-  motion = digitalRead(PIN);
-  if (motion) {
-    if (motion_count == 0) {
-      start_time = millis();
-      // t.fill_screen( TFT_BLACK );
-    }
-    last_time = millis();
-    //    t.lcd_display( 1, 0, t.fontsize, 100, String( last_time ) );
-    motion_count++;
-    motion_count_post++;
-    String mt = "Motion=" + String(motion_count);
-    // t.lcd_display( 1, 30, t.fontsize, 0, mt );
-    Serial.println(mt);
-    // if ( motion_count >= motion_threshold )
-    // {
-    //     // publish_message( device, String( motion_count ) );
-    //     Serial.println("publish_message");
-    // }
-  }
-}
+  StaticJsonDocument<256> doc;
+  doc["temperature"] = bme.temperature;
+  doc["pressure"] = bme.pressure / 100.0;
+  doc["humidity"] = bme.humidity;
+  doc["gas_resistance"] = bme.gas_resistance / 1000.0;
+  doc["approx_altitude"] = bme.readAltitude(SEALEVELPRESSURE_HPA);
 
+  String json;
+  serializeJson(doc, json);
+  server.send(200, "application/json", json);
+}
 
 void getMenu() {
   Serial.println("get Menu");
   String html = "<html><body>";
   html += "<h1>Motion Sensor - Enforcer 1</h1><br>";
   html += "Interval = ";
-  html += SAMPLE_INTERVAL;
-  html += "<br>";
-  html += "<a href='http://10.0.0.21/set?interval=10000'>http://10.0.0.21/set?interval=10000</a><br>";
-  html += "<a href='http://10.0.0.21/motion'>Motion Data</a><br>";
-  html += "<a href='http://10.0.0.21/poll'>Polled Data</a><br>";
-  html += "<a href='http://10.0.0.21/reset'>Reset</a><br>";
+  html += interval;
+  html += "<br><a href='/poll'>Start polling</a>";
+  html += "<br><a href='/motion'>Get motion data</a>";
+  html += "<br><a href='/temp'>Get temperature data</a>";
+  html += "<br><a href='/reset'>Reset Data</a>";
+  html += "<br><a href='/set'>Set Settings</a>";
   html += "</body></html>";
   server.send(200, "text/html", html);
 }
 
+void resetData() {
+  Serial.println("Reset Data");
+  motion_count = 0;
+  motion_count_post = 0;
+  server.send(200, "application/json", "Data Reset");
+}
 
 void getSettings() {
-  Serial.println("getSettings()");
-  // Serial.println( server.args() );
-  String message = "";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-    String name1 = server.argName(i);
-    //      String value1 = server.arg ( i );
-    if (name1.equals("interval")) {
-      SAMPLE_INTERVAL = server.arg(i).toInt();
-      Serial.println(SAMPLE_INTERVAL);
-    }
-  }
-  Serial.println(message);
-  //    if (server.arg("interval")== "true")
-  //    {
-  //        response+= ",\"signalStrengh\": \""+String(WiFi.RSSI())+"\"";
-  //    }
-  DynamicJsonDocument doc1(2048);
-  doc1["set"] = "Great Success !";
-  // Serialize JSON document
+  String html = "<html><body>";
+  html += "<form action=\"/set\" method=\"POST\">";
+  html += "<label for=\"interval\">Set Interval:</label><br>";
+  html += "<input type=\"text\" id=\"interval\" name=\"interval\" value=\"";
+  html += interval;
+  html += "\"><br><br>";
+  html += "<input type=\"submit\" value=\"Submit\">";
+  html += "</form>";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void get_poll() {
+  String html = "<html><body>";
+  html += "Polling started!";
+  html += "</body></html>";
+  server.send(200, "text/html", html);
+}
+
+void get_motion() {
+  StaticJsonDocument<256> doc;
+  doc["motion"] = motion;
+  doc["motion_count"] = motion_count;
+  doc["motion_count_post"] = motion_count_post;
+
   String json;
-  serializeJson(doc1, json);
+  serializeJson(doc, json);
   server.send(200, "application/json", json);
 }
 
-
-
-void add_json_object(char *tag, float value, char *unit) {
-  int dic_size = jsonDocument_1.size();
-  if (dic_size >= MAX_ELEMENTS) {
-    Serial.println("Remove 1");
-    jsonDocument_1.remove(0);
-  }
-  JsonObject objt = jsonDocument_1.createNestedObject();
-  objt["time"] = get_time();
-  objt["type"] = tag;
-  objt["value"] = value;
-  objt["unit"] = unit;
-}
-
-void add_json_object_2(char *tag, float value, char *unit) {
-  int dic_size = jsonDocument_2.size();
-  if (dic_size >= MAX_ELEMENTS) {
-    Serial.println("Remove 1");
-    jsonDocument_2.remove(0);
-  }
-  JsonObject objt = jsonDocument_2.createNestedObject();
-  objt["time"] = get_time();
-  objt["type"] = tag;
-  objt["value"] = value;
-  objt["unit"] = unit;
-}
-
-void show_time() {
-  String t = get_time();
-  tft.fillScreen(TFT_BLACK);
-  tft.setTextColor(TFT_BLUE);
-  tft.drawString(t, 10, 10, 6);
-  Serial.print("time=");
-  Serial.println(t);
+void loop() {
+  loop_bme();
+  server.handleClient();
 }
 
 void read_sensor_data(void *parameter) {
-  for (;;) {
-    String theString = get_time();
-    // Serial.print("theString=");
-    // Serial.println(theString);
-    int theInt = theString.substring(theString.length() - 2, theString.length()).toInt();
-    // Serial.print("theInt=");
-    // Serial.println(theInt);
-    read_sensors();
-    vTaskDelay(SAMPLE_INTERVAL / portTICK_PERIOD_MS);
+  while (true) {
+    if (millis() - last_time > interval) {
+      read_sensors();
+      last_time = millis();
+    }
+    delay(10);
   }
 }
+
 
 void post_sensor_data(void *parameter) {
   for (;;) {
@@ -356,134 +226,13 @@ void post_sensor_data(void *parameter) {
   }
 }
 
-void resetData() {
-  jsonDocument_1.clear();
-  jsonDocument_2.clear();
-  Serial.println("Data reset");
-  DynamicJsonDocument doc1(2048);
-  doc1["reset"] = "Great Success !";
-  String json;
-  serializeJson(doc1, json);
-  server.send(200, "application/json", json);
-}
-
-void get_poll() {
-  Serial.println("Get Polled Data");
-  serializeJson(jsonDocument_1, buffer);
-  Serial.println(buffer);
-  // int dic_size_motion = jsonDocument_1.size();
-  // int dic_size = doc.size();
-  // Serial.println(dic_size_motion);
-  // Serial.println(dic_size);
-
-  // for (int i=0; i < dic_size; i++)
-  // {
-  //   const char* sensor = doc[i]["type"];
-  //   Serial.println( sensor );
-  // }
-  // Serial.println( buffer );
-  server.send(200, "application/json", buffer);
-}
-
-
-void get_motion() {
-  Serial.println("Get Motion Times");
-  StaticJsonDocument<64> filter;
-  filter["type"] = "motion";
-
-  int size = jsonDocument_2.size();
-  Serial.println(size);
-  if (!size) {
-    Serial.println("NULL");
-    server.send(200, "application/json", "{}");
-  }
-
-
-  if (size) {
-    Serial.println("Not NULL");
-    serializeJson(jsonDocument_2, buffer);
-    server.send(200, "application/json", buffer);
-    jsonDocument_2.clear();  //  Reset data for iot client device
-  }
-}
-
-
-void loop() {
-  loop_bme();
-  server.handleClient();
-}
-
-
-String get_time() {
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    Serial.println("Failed to obtain time");
-    return "Error";
-  }
-  //  Serial.println(&timeinfo, "print - %A, %B %d %Y %H:%M:%S");
-  //https://arduino.stackexchange.com/questions/52676/how-do-you-convert-a-formatted-print-statement-into-a-string-variable
-  char timeStringBuff[50];
-  // strftime(timeStringBuff, sizeof(timeStringBuff), "%A, %B %d %Y %H:%M:%S", &timeinfo);
-  strftime(timeStringBuff, sizeof(timeStringBuff), "%H:%M:%S", &timeinfo);
-  String asString(timeStringBuff);
-  return asString;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/***
-  Read Our Complete Guide: https://RandomNerdTutorials.com/bme680-sensor-arduino-gas-temperature-humidity-pressure/
-  
-  Designed specifically to work with the Adafruit BME680 Breakout ----> http://www.adafruit.com/products/3660 These sensors use I2C or SPI to communicate, 2 or 4 pins are required to interface. Adafruit invests time and resources providing this open source code, please support Adafruit and open-source hardware by purchasing products from Adafruit! Written by Limor Fried & Kevin Townsend for Adafruit Industries. BSD license, all text above must be included in any redistribution
- ***/
-
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
-
-/*#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10*/
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME680 bme; // I2C
-//Adafruit_BME680 bme(BME_CS); // hardware SPI
-//Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);
 
 void setup_bme() {
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println(F("BME680 async test"));
-
   if (!bme.begin()) {
-    Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
     while (1);
   }
 
-  // Set up oversampling and filter initialization
   bme.setTemperatureOversampling(BME680_OS_8X);
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
@@ -492,69 +241,56 @@ void setup_bme() {
 }
 
 void loop_bme() {
-  // Tell BME680 to begin measurement.
-  unsigned long endTime = bme.beginReading();
-  if (endTime == 0) {
-    Serial.println(F("Failed to begin reading :("));
-    return;
-  }
-  Serial.print(F("Reading started at "));
-  Serial.print(millis());
-  Serial.print(F(" and will finish at "));
-  Serial.println(endTime);
-
-  Serial.println(F("You can do other work during BME680 measurement."));
-  delay(50); // This represents parallel work.
-  // There's no need to delay() until millis() >= endTime: bme.endReading()
-  // takes care of that. It's okay for parallel work to take longer than
-  // BME680's measurement time.
-
-  // Obtain measurement results from BME680. Note that this operation isn't
-  // instantaneous even if milli() >= endTime due to I2C/SPI latency.
-  if (!bme.endReading()) {
-    Serial.println(F("Failed to complete reading :("));
-    return;
-  }
-  Serial.print(F("Reading completed at "));
-  Serial.println(millis());
-
-  Serial.print(F("Temperature = "));
-  Serial.print(bme.temperature);
-  Serial.println(F(" *C"));
-
-  Serial.print(F("Pressure = "));
-  Serial.print(bme.pressure / 100.0);
-  Serial.println(F(" hPa"));
-
-  Serial.print(F("Humidity = "));
-  Serial.print(bme.humidity);
-  Serial.println(F(" %"));
-
-  Serial.print(F("Gas = "));
-  Serial.print(bme.gas_resistance / 1000.0);
-  Serial.println(F(" KOhms"));
-
-  Serial.print(F("Approx. Altitude = "));
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(F(" m"));
-
-  Serial.println();
-  delay(2000);
+  // Placeholder for any BME680 specific loop actions
 }
 
-void get_temperature()
-{
+void read_sensors() {
   unsigned long endTime = bme.beginReading();
-  if (endTime == 0)
-  {
-    Serial.println(F("Failed to begin reading bme680:("));
+  if (endTime == 0) {
+    Serial.println(F("Failed to begin reading BME680 :("));
     return;
   }
   delay(50);
-  if (!bme.endReading()){
-    Serial.println(F("Failed to complete reading BME680:("));
+  if (!bme.endReading()) {
+    Serial.println(F("Failed to complete reading BME680 :("));
+    return;
   }
 
-  temperature = int(bme.temperature);
-  return temperature;
+  add_json_object("Temperature", bme.temperature, "C");
+  add_json_object("Pressure", bme.pressure / 100.0, "hPa");
+  add_json_object("Humidity", bme.humidity, "%");
+  add_json_object("Gas Resistance", bme.gas_resistance / 1000.0, "kOhms");
+  add_json_object("Approx Altitude", bme.readAltitude(SEALEVELPRESSURE_HPA), "m");
+}
+
+void add_json_object(char *tag, float value, char *unit) {
+  StaticJsonDocument<256> doc;
+  doc["tag"] = tag;
+  doc["value"] = value;
+  doc["unit"] = unit;
+
+  String json;
+  serializeJson(doc, json);
+  Serial.println(json);
+}
+
+void add_json_object_2(char *tag, float value, char *unit) {
+  StaticJsonDocument<256> doc;
+  doc["tag"] = tag;
+  doc["value"] = value;
+  doc["unit"] = unit;
+
+  String json;
+  serializeJson(doc, json);
+  Serial.println(json);
+}
+
+void IRAM_ATTR toggleButton1() {
+  motion = !motion;
+  motion_count++;
+  motion_count_post++;
+}
+
+void IRAM_ATTR toggleButton2() {
+  // Placeholder for Button2 functionality
 }
